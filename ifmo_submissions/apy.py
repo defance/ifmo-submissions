33f5c2b749e76eb8c9e13411970e2d8d7dba950b
Dmitry Ivanyushin <defance@gmail.com>
2016-04-15 18:47:22 +0300

    Initial

---
+++ ifmo_submissions/apy.py
@@ -0,0 +1,39 @@
+from submissions.serializers import (
+    SubmissionSerializer, StudentItemSerializer, ScoreSerializer
+)
+from ifmo_submissions.serializers import ScoreAnnotationSerializer
+from submissions.models import Submission, StudentItem, Score, ScoreAnnotation
+
+
+def get_submission_annotation(student_item, attempt_number):
+
+    # TODO: Docs
+    try:
+        student_item_model = StudentItem.objects.get(**student_item)
+        submission = Submission.objects.get(student_item=student_item_model,
+                                            attempt_number=attempt_number)
+        score = None
+        annotation = None
+
+        try:
+            score = Score.objects.filter(
+                student_item=student_item_model,
+                submission=submission).order_by("-id").first()
+        except Score.DoesNotExist:
+            pass
+
+        if score is not None:
+            try:
+                annotation = ScoreAnnotation.objects.get(score=score)
+            except ScoreAnnotation.DoesNotExist:
+                pass
+
+    except StudentItem.DoesNotExist, Submission.DoesNotExist:
+        return None
+
+    return {
+        'student_module': StudentItemSerializer(student_item_model).data,
+        'submission': SubmissionSerializer(submission).data,
+        'score': ScoreSerializer(score).data if score is not None else None,
+        'annotation': ScoreAnnotationSerializer(annotation).data if annotation is not None else None,
+    }
---
+++ ifmo_submissions/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from submissions.models import ScoreAnnotation
+
+
+class ScoreAnnotationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ScoreAnnotation
+        fields = (
+            'score',
+            'annotation_type',
+            'creator',
+            'reason',
+        )                                                               )
+
