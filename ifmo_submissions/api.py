e2dd75d2e101b0b4dfb689cad49350c172ac1ee7
Dmitry Ivanyushin <defance@gmail.com>
2016-05-05 17:24:46 +0300

    Получение Annotation из апи

--- ifmo_submissions/api.py
+++ ifmo_submissions/api.py
@@ -2,7 +2,7 @@ from submissions.serializers import (
     SubmissionSerializer, StudentItemSerializer, ScoreSerializer
 )
 from ifmo_submissions.serializers import ScoreAnnotationSerializer
-from submissions.models import Submission, StudentItem, Score, ScoreAnnotation
+from submissions.models import Submission, StudentItem, Score, ScoreAnnotation, ScoreSummary
 
 
 def get_submission_annotation(student_item, attempt_number):
@@ -71,3 +71,34 @@ def get_submission_annotation_uuid(uuid):
         'annotation': ScoreAnnotationSerializer(annotation).data if annotation is not None else None,
     }
 
+
+def get_annotation(student_item):
+    """Get the annotation for a particular student item
+
+    This function will return the annotation if it is available.
+
+    Args:
+        student_item (dict): The dictionary representation of a student item.
+            Function returns the annotation related to this student item.
+
+    Returns:
+        annotation (dict): The annotation associated with this student item.
+            None if there is no annotation found.
+
+    Raises:
+        SubmissionInternalError: Raised if a score cannot be retrieved because
+            of an internal server error.
+    """
+    try:
+        student_item_model = StudentItem.objects.get(**student_item)
+        score = ScoreSummary.objects.get(student_item=student_item_model).latest
+        annotation = ScoreAnnotation.objects.get(score=score)
+    except (ScoreSummary.DoesNotExist, StudentItem.DoesNotExist, ScoreAnnotation.DoesNotExist):
+        return None
+
+    # By convention, scores are hidden if "points possible" is set to 0.
+    # This can occur when an instructor has reset scores for a student.
+    if score.is_hidden():
+        return None
+    else:
+        return ScoreAnnotationSerializer(annotation).data
