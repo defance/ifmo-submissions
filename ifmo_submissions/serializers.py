a04acbc93f004b6867fa82e988463c6c1816860c
Dmitry Ivanyushin <defance@gmail.com>
2017-02-15 17:20:52 +0300

    Починена сериализация ScoreAnnotation

--- ifmo_submissions/serializers.py
+++ ifmo_submissions/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from submissions.models import ScoreAnnotation
+from submissions.serializers import ScoreSerializer
 
 
 class ScoreAnnotationSerializer(serializers.ModelSerializer):
+
+    score = ScoreSerializer()
 
     class Meta:
         model = ScoreAnnotation
